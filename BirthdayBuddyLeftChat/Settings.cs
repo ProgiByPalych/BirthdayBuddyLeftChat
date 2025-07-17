7518f2005149aae1248512e7125b1290de418a10
Большаков Сергей <bolshakov.s@madin.ru>
2025-07-18 00:21:31 +0300

    create project

---
+++ BirthdayBuddyLeftChat/Settings.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.Json;
+
+namespace SemenNewsBot
+{
+    public class Settings
+    {
+        private static Settings? instance;
+        public static Settings Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Settings();
+                return instance;
+            }
+        }
+
+        private static readonly string _settingsPath = "Settings.json";
+        public string GetSettingsPath() { return _settingsPath; }
+        public string? TokenToAccess { get; set; }
+        public long SemenovChatId { get; set; }
+        public int? SemenovThemeId { get; set; }
+
+        public static void Init()
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(Settings.Instance.GetSettingsPath()))
+                {
+                    string json = reader.ReadToEnd();
+                    Settings.instance = JsonSerializer.Deserialize<Settings>(json);
+                    Console.WriteLine("Read TokenToAccess: " + Settings.Instance.TokenToAccess);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Save();
+            }
+        }
+        public static void Save()
+        {
+            // полная перезапись файла 
+            using (StreamWriter writer = new StreamWriter(Settings.Instance.GetSettingsPath(), false))
+            {
+                writer.WriteLine(JsonSerializer.Serialize(Settings.Instance, typeof(Settings)));
+            }
+        }
+
+        public static void Add()
+        {
+            // добавление в файл
+            using (StreamWriter writer = new StreamWriter(Settings.Instance.GetSettingsPath(), true))
+            {
+                writer.WriteLine("Addition");
+                writer.Write("4,5");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 # BirthdayBuddyLeftChat
+
+## Подготовка СЕ-container в ProxMox
+Установим способ входа под root правами временно.
+
+```bash
+nano /etc/ssh/sshd_config
+```
+
+Должны быть раскоментированы следующие строки:
+
+```bash
+Include /etc/ssh/sshd_config.d/*.conf
+PermitRootLogin yes
+PubkeyAuthentication yes
+PasswordAuthentication yes
+KbdInteractiveAuthentication no
+UsePAM yes
+X11Forwarding yes
+PrintMotd no
+AcceptEnv LANG LC_*
+Subsystem       sftp    /usr/lib/openssh/sftp-server
+```
+
+Далее перезагружаемся:
+
+```bash
+reboot
+```
+
+Обновляем репозиторий:
+
+```bash
+sudo apt-get update
+```
+
+Устанавливаем зависимости:
+
+```bash
+sudo apt-get install -y dotnet-sdk-8.0
+
+sudo apt-get install -y aspnetcore-runtime-8.0
+```
+
+Проверяем установленную версию dotnet:
+
+```bash
+dotnet --version
+
+dotnet --list-sdks
+```
+
+Копируем настройки запуска сервиса ```BirthdayBuddyLeftChat.service``` по пути ```/etc/systemd/system/BirthdayBuddyLeftChat.service```
+
+Обновляем сервисы:
+
+```bash
+systemctl daemon-reload
+```
+
+Создаем папку ```/root/publish/BirthdayBuddyLeftChat```
+
+Ну и собственно можно сделать **Publish**
